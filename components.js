702ee1b505ccbea9497a89b256f0756f05813979
RubensJr21 <junior.correa2001@gmail.com>
2022-01-18 14:09:01 -0300

    :recycle: Refatora parte das dependências e dos conflitos
    Refatora as funções que criam os Boxes de dependências e conflitos, as funções que criam as dependências e os conflitos, e a função de populatePreview

--- components.js
+++ components.js
@@ -4,7 +4,7 @@ var observer = new MutationObserver(function(mutations) {
             mutation.target.update()
         }
     });
-  });
+});
 
 class Box extends HTMLElement {
     constructor(){
@@ -129,12 +129,15 @@ class Box extends HTMLElement {
                 return div
             },
             
-            createDependencie: (dependencie) => {
+            createDependencie: (dependencie, config = {isPreview: false}) => {
                 var div = document.createElement("div")
                 div.id = dependencie
                 div.classList.add("divSpanDepenID")
-                div.title = "Clique duas vezes para deletar"
                 div.textContent = dependencie
+
+                if(config.isPreview) return div
+
+                div.title = "Clique duas vezes para deletar"
                 div.onmousemove = (event) => {
                     const x = event.x
                     const y = event.y
@@ -154,7 +157,7 @@ class Box extends HTMLElement {
                     const input = document.querySelector("#viewDC")
                     if (!input.checked){
                         input.checked = true
-                        this.populatePreview(viewDC, div)
+                        this.functions.populatePreview(viewDC, div)
                         viewDC.style.setProperty("--borderColor", "#6eda2c")
                     }
                 }
@@ -174,12 +177,19 @@ class Box extends HTMLElement {
                 }
                 return div
             },
-            createDependenciesBox: () => {
+            createDependenciesBox: (config = {isPreview: false}) => {
                 var fieldset = document.createElement("fieldset")
+
                 var legend = document.createElement("legend")
                 legend.innerText = "Dependências:"
+
                 var div = document.createElement("div")
                 div.id = "divDepenID"
+
+                fieldset.appendChild(legend)
+                fieldset.appendChild(div)
+
+                if(config.isPreview) return fieldset
             
                 var input = document.createElement("input")
                 input.type = "text"
@@ -193,23 +203,24 @@ class Box extends HTMLElement {
                         const element = document.getElementById(e.target.value)
                         if(element && !this.dependencies.includes(element)) {
                             this.addDependencies(element)
-                            this.updateDependencies()
                             element.addDependent(this)
                         }
                     }
                 }
-    
-                fieldset.appendChild(legend)
-                fieldset.appendChild(div)
+
                 fieldset.appendChild(input)
                 return fieldset
             },
             
-            createConflict: (conflict) => {
+            createConflict: (conflict, config = {isPreview: false}) => {
                 var div = document.createElement("div")
                 div.id = conflict
                 div.classList.add("divSpanConfliID")
                 div.textContent = conflict
+                
+                if(config.isPreview) return div
+
+                div.title = "Clique duas vezes para deletar"
                 div.onmousemove = (event) => {
                     const x = event.x
                     const y = event.y
@@ -229,7 +240,7 @@ class Box extends HTMLElement {
                     const input = document.querySelector("#viewDC")
                     if (!input.checked){
                         input.checked = true
-                        this.populatePreview(viewDC, div)
+                        this.functions.populatePreview(viewDC, div)
                         viewDC.style.setProperty("--borderColor", "#da0001")
                     }
                 }
@@ -244,20 +255,27 @@ class Box extends HTMLElement {
                     })
                     const conflict = document.getElementById(e.path[0].id)
                     this.remConflicts(conflict)
-                    // Esconde o Preview
+                    // Esconde o Preview após remoção
                     const viewDC = document.querySelector("#viewDC")
                     viewDC.checked = false
                 }
                 return div
             },
     
-            createConflictsBox: () => {
+            createConflictsBox: (config = {isPreview: false}) => {
                 var fieldset = document.createElement("fieldset")
+
                 var legend = document.createElement("legend")
                 legend.innerText = "Conflitos:"
+
                 var div = document.createElement("div")
                 div.id = "divConfliID"
+                
+                fieldset.appendChild(legend)
+                fieldset.appendChild(div)
             
+                if(config.isPreview) return fieldset
+
                 var input = document.createElement("input")
                 input.type = "text"
                 input.name = "conflitos"
@@ -270,17 +288,12 @@ class Box extends HTMLElement {
                         const element = document.getElementById(e.target.value)
                         if(element && !this.conflicts.includes(element)){
                             this.addConflicts(element)
-                            const div = this.shadowRoot.getElementById("divConfliID")
-                            div.appendChild(this.functions.createConflict(e.target.value))
                             element.addConflicting(this)
                         }
                     }
                 }
             
-                fieldset.appendChild(legend)
-                fieldset.appendChild(div)
                 fieldset.appendChild(input)
-            
                 return fieldset
             },
     
@@ -318,6 +331,49 @@ class Box extends HTMLElement {
                     })
                 }
                 return button
+            },
+
+            populatePreview: (v, currentElement) => {
+                const element = document.querySelector(`#${currentElement.textContent}`)
+                
+                const info = element.getInfos()
+        
+                const id = document.createElement("h1")
+                id.textContent = `ID: ${info.id}`
+        
+                const nome = document.createElement("h2")
+                nome.textContent = `Nome: ${info.nome}`
+        
+                const descricao = document.createElement("h2")
+                descricao.textContent = `Descrição: ${info["descrição"]}`
+
+                const dependencies = this.functions.createDependenciesBox({ isPreview: true })
+                const divDepenID = dependencies.querySelector("div#divDepenID")
+                info["dependências"].forEach((element, index, array) => {
+                    divDepenID.appendChild(this.functions.createDependencie(element.id, {isPreview: true}))
+                })
+                dependencies.appendChild(divDepenID)
+        
+                const conflitos = this.functions.createConflictsBox({ isPreview: true })
+                var divConfliID = conflitos.querySelector("div#divConfliID")
+                info.conflitos.forEach((element, index, array) => {
+                    divConfliID.appendChild(this.functions.createConflict(element.id, { isPreview }))
+                })
+                conflitos.appendChild(divConfliID)
+        
+                v.innerHTML = ""
+                v.appendChild(id)
+                v.appendChild(nome)
+                v.appendChild(descricao)
+                v.appendChild(dependencies)
+                v.appendChild(conflitos)
+                const idPure = currentElement.id.replace(/[^A-Z]/g, "")
+                const tagNames = {
+                    "RF": "green",
+                    "RNF": "orangered",
+                    "RN": "indigo"
+                }
+                v.style.setProperty("--backgroundColor", tagNames[idPure])
             }
         }
     }
@@ -333,70 +389,10 @@ class Box extends HTMLElement {
         }
     }
 
-    populatePreview (v, currentElement) {
-        const element = document.querySelector(`#${currentElement.textContent}`)
-        
-        const info = element.getInfos() 
-
-        const id = document.createElement("h1")
-        id.textContent = `ID: ${info.id}`
-
-        const nome = document.createElement("h2")
-        nome.textContent = `Nome: ${info.nome}`
-
-        const descricao = document.createElement("h2")
-        descricao.textContent = `Descrição: ${info["descrição"]}`
-                            
-        const dependencies = document.createElement("fieldset")
-        var legendDepen = document.createElement("legend")
-        legendDepen.innerText = "Dependências:"
-        var divDepenID = document.createElement("div")
-        divDepenID.id = "divDepenID"
-        info["dependências"].forEach((element, index, array) => {
-            var div = document.createElement("div")
-            div.id = element.id
-            div.classList.add("divSpanDepenID")
-            div.textContent = element.id
-            divDepenID.appendChild(div)
-        })
-        dependencies.appendChild(legendDepen)
-        dependencies.appendChild(divDepenID)
-
-        const conflitos = document.createElement("fieldset")
-        var legendConf = document.createElement("legend")
-        legendConf.innerText = "Conflitos:"
-        var divConfliID = document.createElement("div")
-        divConfliID.id = "divConfliID"
-        info.conflitos.forEach((element, index, array) => {
-            var div = document.createElement("div")
-            div.id = element.id
-            div.classList.add("divSpanConfliID")
-            div.textContent = element.id
-            divConfliID.appendChild(div)
-        })
-        conflitos.appendChild(legendConf)
-        conflitos.appendChild(divConfliID)
-
-        window.parentElement = this.parentElement
-        const root = this.parentElement
-        v.innerHTML = ""
-        v.appendChild(id)
-        v.appendChild(nome)
-        v.appendChild(descricao)
-        v.appendChild(dependencies)
-        v.appendChild(conflitos)
-        const idPure = currentElement.id.replace(/[^A-Z]/g, "")
-        const tagNames = {
-            "RF": "green",
-            "RNF": "orangered",
-            "RN": "indigo"
-        }
-        v.style.setProperty("--backgroundColor", tagNames[idPure])
-    }
-
     addDependencies(element){
         console.log(`${this.id}:`, this, "depende de", element)
         this.dependencies.push(element)
+        this.updateDependencies()
     }
     remDependencies(element){
         console.log(`${this.id}:`, this, "não depende mais de", element)
@@ -433,6 +429,7 @@ class Box extends HTMLElement {
     addConflicts(element){
         console.log(`${this.id}:`, this, "conflita com", element)
         this.conflicts.push(element)
+        this.updateConflicts()
     }
     remConflicts(element){
         console.log(`${this.id}:`, this, "não conflita mais com", element)
@@ -551,7 +548,6 @@ ${types[type] || ""}
             this.shadowRoot.getElementById("id").textContent = this.id
             this.shadowRoot.querySelector("div").id = this.id
         }
-        // console.log(this)
         setID()
     }
 }
